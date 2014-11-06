42ed7a7e0af07082aa99730b9b5f03673a7cf4a1
cxoltero <carlosh95@gmail.com>
2014-11-05 19:58:36 -0500

    cleaned up code inside app.js

--- js/app.js
+++ js/app.js
@@ -1,50 +1,30 @@
 
 var Router = Backbone.Router.extend({
-    routes: {
-        '': 'home',
-        'home': 'home',
-        'about': 'about',
-        'submissions': 'submissions',
-        'comments': 'comments',
-        'users' : 'users'
-    },
-
+  routes: {
+    '': 'home',
+    'home': 'home',
+    'about': 'about',
+    'submissions': 'submissions',
+    'comments': 'comments',
+    'users' : 'users'
+  },
 
 //#################################################
-    // home: function(){
-    //   $("#content").empty();
 
-    //   $.ajax({
-    //     url: 'https://hackernewsch.herokuapp.com/submissions',
-    //     type: 'GET'
-    //   }).done(function(response) {
-    //     console.table(response);
-
-    //     var template = Handlebars.compile($("#homeTemplate").html());
-    //     $("#content").html(template({
-    //       submissionList: response
-
-    //     }));
-    //   }).fail(function(jqXHR, textStatus, errorThrown){
-    //     trace(jqXHR, textStatus, errorThrown);
-    //   });
-    // },
-home: function() {
+  home: function() {
     $('#content').empty();
 
     $.ajax({
       url: 'https://hackernewsch.herokuapp.com/submissions',
       type: 'GET'
     }).done(function(response) {
-      console.table(response);
 
-      var template = Handlebars.compile($("#submissionsTemplate").html());
+    var template = Handlebars.compile($("#submissionsTemplate").html());
       $('#content').html(template({
         submissionsList: response
       }));
     });
   },
-
 
 //####################################################
 
@@ -57,34 +37,28 @@ home: function() {
     //   }));
     // },
 //######## we can do the same thing with or withour handlebar templates! ###########
-    about: function(){
-      var name = "Carlos"
+  about: function(){
+    $('#content').empty();
+    var name = "Carlos"
 
-      $('#content').html('<h1> About </h1><p>Hello ' + name + ' </p>')
+    $('#content').html('<h1> About </h1><p>Hello ' + name + ' </p>')
     },
+//############################################################################
+  comments: function(){
+    $('#content').empty();
 
+  },
+//############################################################################
+  users: function() {
+    $('#content').empty();
 
+    var template = Handlebars.compile($("#usersTemplate").html());
+    $('#content').html(template({
+        user: 'Carlos'
+    }));
 
-    comments: function(){
-      $('#content').empty();
-
-    },
-
-    // submissions: function(){
-    //   $('#content').empty();
-
-    // },
-
-    users: function() {
-      $('#content').empty();
-
-      var template = Handlebars.compile($("#usersTemplate").html());
-      $('#content').html(template({
-          user: 'Carlos'
-      }));
-
-    },
-
+  },
+//############################################################################
 });
 
 var router = new Router();
